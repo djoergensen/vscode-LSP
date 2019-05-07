d6e9ac81858a9fecc9cc22ddf763b9e7219b4f4e
Johan Pedersen <JohanPedersen@Deltek.com>
2019-05-07 14:36:25 +0200

    Prepared for unit tests

---
+++ client/src/test/unit/unit.test.ts
@@ -0,0 +1,12 @@
+import * as assert from 'assert';
+import * as client from "../../extension";
+
+
+
+describe('Suite', function() {
+	describe('Client', function () {
+	  it('This is where unit test of the client go', function () {
+			assert.equal(1,1);
+	  });
+	});	
+});
